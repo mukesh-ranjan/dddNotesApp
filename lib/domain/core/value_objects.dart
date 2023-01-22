188706ac491a7b328731aadded337b4fe09926ae
unknown <ranjan.muku@gmail.com>
2023-01-22 20:25:29 +0530

    Step-04:Firebase Facade Implementation For Email Authentication and Google Authentication

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import '../../domain/core/errors.dart';
 import '../../domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -8,6 +9,12 @@ abstract class ValueObject<T> {
   const ValueObject();
 
   Either<ValueFailure<T>, T> get value;
+
+  /// Throw [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    //id = identity - same as writing (right)=>right | (r) => r
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight();
 
